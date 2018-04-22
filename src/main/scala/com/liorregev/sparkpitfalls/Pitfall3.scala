4bc00c5c324458badbe2e0f32daabda9ce88cf13
Lior Regev <lioregev@gmail.com>
2018-04-22 17:17:43 +0300

    Added Pitfall3

---
+++ src/main/scala/com/liorregev/sparkpitfalls/Pitfall3.scala
@@ -0,0 +1,49 @@
+package com.liorregev.sparkpitfalls
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{functions => F}
+
+object Pitfall3 extends Pitfall {
+  import spark.implicits._
+
+  val N = 10000
+
+  val ds = spark.createDataset(0 to N).repartition(100).sort($"value" asc)
+  spark.sparkContext.setJobDescription("Calculate using window")
+  val differencesWindowed = ds
+    .select($"value" - (F.lag($"value", 1) over Window.orderBy($"value" asc)))
+    .as[Option[Int]]
+    .collect()
+    .flatten
+  spark.sparkContext.setJobDescription("Calculate using RDD")
+  val differencesRdd = {
+    val lastValues = spark.sparkContext.broadcast(
+      ds.rdd
+        .mapPartitionsWithIndex((idx, it) => {
+          Seq(it.foldLeft(idx -> 0) {
+            case (_, newValue) => idx -> newValue
+          }).iterator
+        })
+        .collect()
+        .toMap
+    )
+
+    ds.rdd
+      .mapPartitionsWithIndex((idx, it) => {
+        if(!it.hasNext) {
+          Iterator.empty
+        } else {
+          val previousPartitionLastValue = lastValues.value.get(idx - 1)
+
+          (Seq(previousPartitionLastValue).flatten.iterator ++ it)
+            .sliding(2)
+            .map(_.reverse.reduce(_ - _))
+        }
+      })
+      .collect()
+      .drop(1)
+  }
+
+  println("Ready")
+  Thread.sleep(60000)
+}
